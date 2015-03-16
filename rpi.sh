c7b6f389c11eaa8bfcfdbb6b5171f9a8f5d411df
Michael Buhler <michael@michaelbuhler.com>
2015-03-16 19:27:37 -0400

    corrected RPiV1 gcc command line parameters

--- rpi.sh
+++ rpi.sh
@@ -2,7 +2,7 @@
 dir=$(pwd)
 base=$(dirname $0)
 cd $base/src
-arm-none-eabi-gcc -O2 -mfpu=neon-vfpv4 -mfloat-abi=hard -mcpu=cortex-a7 -nostartfiles -g ecu.c -o kernel.elf
+arm-none-eabi-gcc -O2 -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -nostartfiles -g ecu.c -o kernel.elf
 mv kernel.elf ../bin/
 cd ../bin/
 arm-none-eabi-objcopy kernel.elf -O binary kernel.img
