cc8dc1e9186530cb75b024e23b89852da6fe4da5
Michael Buhler <michael@michaelbuhler.com>
2015-03-18 22:58:42 -0400

    removed compiler optimization; causes more headaches than it's worth

--- rpi.sh
+++ rpi.sh
@@ -2,6 +2,6 @@
 dir=$(pwd)
 base=$(dirname $0)
 cd $base
-arm-none-eabi-gcc -g -O2 -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -nostartfiles -Wl,-T,src/ld/armelf.x -o bin/kernel.elf src/asm/startup.S src/cstartup.c src/ecu.c
+arm-none-eabi-gcc -g -mfpu=vfp -mfloat-abi=hard -march=armv6zk -mtune=arm1176jzf-s -nostartfiles -Wl,-T,src/ld/armelf.x -o bin/kernel.elf src/asm/startup.S src/cstartup.c src/ecu.c
 arm-none-eabi-objcopy bin/kernel.elf -O binary bin/kernel.img
 cd $dir
