93f84955c2bf7f6698f5ded0623274404f06e31b
Michael Buhler <michael@michaelbuhler.com>
2015-11-13 19:06:09 -0500

    commit old shiii

--- ecu/src/lib/gpio.h
+++ ecu/src/lib/gpio.h
@@ -15,6 +15,23 @@
     #define GPIO_CLR1  0x3F20002C
     #define GPIO_LEV0  0x3F200034
     #define GPIO_LEV1  0x3F200038
+    #define GPIO_EDS0  0x3F200040
+    #define GPIO_EDS1  0x3F200044
+    #define GPIO_REN0  0x3F20004C
+    #define GPIO_REN1  0x3F200050
+    #define GPIO_FEN0  0x3F200058
+    #define GPIO_FEN1  0x3F20005C
+    #define GPIO_HEN0  0x3F200064
+    #define GPIO_HEN1  0x3F200068
+    #define GPIO_LEN1  0x3F200070
+    #define GPIO_LEN0  0x3F200074
+    #define GPIO_AREN0 0x3F20007C
+    #define GPIO_AREN1 0x3F200080
+    #define GPIO_AFEN0 0x3F200088
+    #define GPIO_AFEN1 0x3F20008C
+    #define GPIO_PUD   0x3F200094
+    #define GPIO_PUDCLK0 0x3F200098
+    #define GPIO_PUDCLK1 0x3F20009C
 #else
     #define GPIO_BASE  0x20200000
     #define GPIO_FSEL0 0x20200000
@@ -29,6 +46,23 @@
     #define GPIO_CLR1  0x2020002C
     #define GPIO_LEV0  0x20200034
     #define GPIO_LEV1  0x20200038
+    #define GPIO_EDS0  0x20200040
+    #define GPIO_EDS1  0x20200044
+    #define GPIO_REN0  0x2020004C
+    #define GPIO_REN1  0x20200050
+    #define GPIO_FEN0  0x20200058
+    #define GPIO_FEN1  0x2020005C
+    #define GPIO_HEN0  0x20200064
+    #define GPIO_HEN1  0x20200068
+    #define GPIO_LEN1  0x20200070
+    #define GPIO_LEN0  0x20200074
+    #define GPIO_AREN0 0x2020007C
+    #define GPIO_AREN1 0x20200080
+    #define GPIO_AFEN0 0x20200088
+    #define GPIO_AFEN1 0x2020008C
+    #define GPIO_PUD   0x20200094
+    #define GPIO_PUDCLK0 0x20200098
+    #define GPIO_PUDCLK1 0x2020009C
 #endif
 
 #define GPIO_INPUT (char)(0)
