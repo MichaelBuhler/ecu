34842b7eb734ef8731284ef14c9c64df9b0c827c
Michael Buhler <michael@michaelbuhler.com>
2015-11-13 21:56:21 -0500

    flash rpi2 onboard led

--- ecu/src/bootstrap.s
+++ ecu/src/bootstrap.s
@@ -2,5 +2,18 @@
 _start:
     mov sp, #0x8000
     bl notmain
-hang:
-    b hang
+    hang: b hang
+
+.global noop
+noop:
+    bx lr
+
+.global get32
+get32:
+    ldr r0, [r0]
+    bx lr
+
+.global put32
+put32:
+    str r1, [r0]
+    bx lr
--- ecu/src/notmain.c
+++ ecu/src/notmain.c
@@ -1,1 +1,26 @@
-void notmain(void) {}
+extern void put32 ( unsigned int, unsigned int );
+extern unsigned int get32 ( unsigned int );
+extern void noop ( unsigned int );
+
+#define GPFSEL4 0x3F200010
+#define GPSET1  0x3F200020
+#define GPCLR1  0x3F20002C
+
+int notmain(void) {
+    unsigned int ra;
+
+    ra = get32(GPFSEL4);
+    ra &= ~(7<<18);
+    ra |= 1<<18;
+    put32(GPFSEL4,ra);
+
+    while (1) {
+        put32(GPSET1,1<<15);
+        for ( ra = 0 ; ra < 0x100000 ; ra++ )
+            noop(ra);
+        put32(GPCLR1,1<<15);
+        for ( ra = 0 ; ra < 0x100000 ; ra++ )
+            noop(ra);
+    }
+    return 0;
+}
