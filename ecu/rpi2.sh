b564beaa3ba9fba29463ec73159aa7b133e67216
Michael Buhler <michael@michaelbuhler.com>
2015-11-13 22:36:04 -0500

    expand gpio.h to support more gpio registers and the rpi and rpi2

---
+++ ecu/rpi2.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+dir=$(pwd)
+base=$(dirname $0)
+cd $base
+arm-none-eabi-as src/bootstrap.s -o out/bootstrap.o
+arm-none-eabi-gcc -DRPI2 -O2 -c src/notmain.c -o out/notmain.o
+arm-none-eabi-ld -Ttext 0x00008000 out/bootstrap.o out/notmain.o -o bin/kernel.elf
+arm-none-eabi-objcopy bin/kernel.elf -O binary bin/kernel.bin
+cd $dir
--- ecu/src/notmain.c
+++ ecu/src/notmain.c
@@ -1,24 +1,22 @@
+#include "gpio.h"
+
 extern void put32 ( unsigned int, unsigned int );
 extern unsigned int get32 ( unsigned int );
 extern void noop ( unsigned int );
 
-#define GPFSEL4 0x3F200010
-#define GPSET1  0x3F200020
-#define GPCLR1  0x3F20002C
-
 int notmain(void) {
     unsigned int ra;
 
-    ra = get32(GPFSEL4);
+    ra = get32(GPIO_FSEL4);
     ra &= ~(7<<18);
     ra |= 1<<18;
-    put32(GPFSEL4,ra);
+    put32(GPIO_FSEL4,ra);
 
     while (1) {
-        put32(GPSET1,1<<15);
+        put32(GPIO_SET1,1<<15);
         for ( ra = 0 ; ra < 0x100000 ; ra++ )
             noop(ra);
-        put32(GPCLR1,1<<15);
+        put32(GPIO_CLR1,1<<15);
         for ( ra = 0 ; ra < 0x100000 ; ra++ )
             noop(ra);
     }
