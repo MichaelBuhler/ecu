9dd032a782fc7e225b648907e86825794bd1f75a
Michael Buhler <michael@michaelbuhler.com>
2015-11-14 10:52:03 -0500

    turn onboard led on/off when GPIO21 is pulled high or low

--- ecu/src/ecu.c
+++ ecu/src/ecu.c
@@ -2,14 +2,10 @@
 #include "lib/gpio.c"
 
 void ecu(void) {
-    unsigned int ra;
-    
+    gpio_mode(21,0);
     gpio_mode(47,1);
     
     while (1) {
-        gpio_write(47,1);
-        for ( ra = 0 ; ra < 0x100000 ; ra++ ) noop(ra);
-        gpio_write(47,0);
-        for ( ra = 0 ; ra < 0x100000 ; ra++ ) noop(ra);
+        gpio_write(47, gpio_read(21) );
     }
 }
