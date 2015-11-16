6e5bd51ca2d68f7fe70e10f16ebf9c8d790a4b1b
Michael Buhler <michael@michaelbuhler.com>
2015-11-15 20:41:49 -0500

    consolidate build scripts, build for rpi and rpi2

--- ecu/rpi2.sh
+++
@@ -1,9 +0,0 @@
-#/bin/sh
-dir=$(pwd)
-base=$(dirname $0)
-cd $base
-arm-none-eabi-as src/bootstrap.s -o out/bootstrap.o
-arm-none-eabi-gcc -DRPI2 -O2 -c src/ecu.c -o out/ecu.o
-arm-none-eabi-ld -T linker_script out/bootstrap.o out/ecu.o -o bin/kernel7.elf
-arm-none-eabi-objcopy bin/kernel7.elf -O binary bin/kernel7.bin
-cd $dir
