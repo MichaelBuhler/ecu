a152e37fb219b79fd4fea1f2b33a5bb9eeced9ea
Michael Buhler <michael@michaelbuhler.com>
2015-11-17 22:21:33 -0500

    improve Arduino crankshaft position sensor emulator

--- cps/cps.ino
+++ cps/cps.ino
@@ -1,18 +1,31 @@
+#define OUTPUT_PIN 2
+#define RPM 30;
+
 unsigned int us;
-byte i;
+unsigned int times;
+unsigned int i;
+unsigned int j;
 
 void setup() {
-  us = 833;
+  us = 833333 / RPM;
+  times = us / 16383 + 1;
+  us /= times;
   pinMode(2,OUTPUT);
 }
 
 void loop() {
   for ( i = 0 ; i < 35 ; i++ ) {
-    digitalWrite(2,HIGH);
-    delayMicroseconds(us);
-    digitalWrite(2,LOW);
-    delayMicroseconds(us);
+    digitalWrite(OUTPUT_PIN,HIGH);
+    pause();
+    digitalWrite(OUTPUT_PIN,LOW);
+    pause();
   }
-  delayMicroseconds(us);
-  delayMicroseconds(us);
+  pause();
+  pause();
 }
+
+void pause() {
+  for ( j = 0 ; j < times ; j++ )
+    delayMicroseconds(us);
+}
+
